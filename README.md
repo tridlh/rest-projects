adad96e8f606bfae1d9896f393b1643343240639
Han Lu <tridlh@gmail.com>
2016-10-31 11:31:50 +0800

    Creating a simple RESTful web service with Python2

--- README.md
+++ README.md
@@ -3,3 +3,4 @@ RESTful service implements
 
 rest-java.sh: creating a "hello world" RESTful web service with Spring.
 rest-nodejs.sh: creating a simple RESTful web service with Node.js.
+rest-python.sh: creating a simple RESTful web service with Python2.
---
+++ rest-python.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+# The script implements the process of creating a simple RESTful web service with python2.
+# Environment:
+#   Ubuntu 16.04 LTS
+# Reference:
+#   http://www.dreamsyssoft.com/python-scripting-tutorial/create-simple-rest-web-service-with-python.php
+
+WORKDIR=~/rest-python
+
+echo "==================Phase 1 of 2: set environment=========================="
+sudo apt update
+cd ~
+sudo apt install vim git build-essential python
+sudo easy_install web.py
+mkdir -p $WORKDIR
+cd $WORKDIR
+git clone https://github.com/tridlh/rest-python.git
+cd rest-python
+echo "==================Phase 2 of 2: run service=============================="
+echo "Use any of the following URLs to test the service:"
+echo "    http://localhost:8080/users"
+echo "    http://localhost:8080/users/1"
+echo "    http://localhost:8080/users/2"
+echo "    http://localhost:8080/users/3"
+./rest.py
